262de4576dc3e5edd43ef0215f55d55086c238a7
dariodaprile <dario.daprile@gmail.com>
2013-06-25 10:01:38 +0100

    connected with instagram API

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org/'
 gem 'sinatra'
 gem 'sinatra-flash'
 gem 'instagram'
+gem 'json'
 
 group :development do
   gem 'shotgun'
--- application.rb
+++ application.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
 require 'sinatra/flash'
 require 'instagram'
+require 'json'
 
 Instagram.configure do |config|
   config.client_id = ENV['INSTAGRAM_ID']
+  config.client_secret = ENV['INSTAGRAM_SECRET']
 end
 
 
@@ -11,6 +13,14 @@ class CatsAndDogs < Sinatra::Base
 
   get '/' do
     "Hello world"
+    dog_photos = Instagram.tag_recent_media('dog')
+    dog_photos.each do |photo|
+      puts photo.images.standard_resolution.url
+    end
   end
 
+# puts Instagram.tag('cat')
+# @a = Instagram.tag_recent_media('dog')
+# json = File.read(@a)
+# puts JSON.parse(json)
 end
