37b44277c389f94f709673f9fc1ae96926c31cad
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-06-29 11:37:37 +0100

    Add further tests

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -24,23 +24,57 @@ describe 'home page', :type => :feature do
     page.should have_css('.search_form')
   end
 
-   it "should show the first_name if user is logged in" do
+  it "should show the first_name if user is logged in" do
     signup_user
     page.should have_content("Dario")
+  end
+
+  it "should log the user in if they provide correct details" do
+    signup_user
+    click_button "Logout"
+    click_link 'Login'
+    fill_in "email", :with => "dario@gmail.com"
+    fill_in "password", :with => "tomtom"
+    click_button "Login"
+    page.should have_content("Dario")
+  end
+
+  it "should not log the user in if they provide incorrect details" do
+    signup_user
+    click_button "Logout"
+    click_link 'Login'
+    fill_in "email", :with => "dario@gmail.com"
+    fill_in "password", :with => "omtom"
+    click_button "Login"
+    page.should have_content("You're email and/or password is wrong.")
+  end
+
+  it "should log out the user" do
+    signup_user
+    click_button "Logout"
+    page.should have_no_content("Dario")
   end
 
 end
 
 describe 'signup', :type => :feature, :js => true do
 
-  it "should not submit if email is not in valid format" do
+  it "should not submit if the email is not in valid format" do
     user = User.new
     user.email = "tomtomtom"
     visit '/signup'
     fill_in "email", :with => user.email
     click_button "Sign Up"
-
     page.should have_content("Please enter a valid email")
+  end
+
+  it "should not submit if the password is too short" do
+    user = User.new
+    user.password = "tomt"
+    visit '/signup'
+    fill_in "email", :with => user.password
+    click_button "Sign Up"
+    page.should have_content("Your password must be a minimum of six character long")
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,4 +20,6 @@ def signup_user
     fill_in "email", :with => user.email
     fill_in "password", :with => user.password
     click_button "Sign Up"
-end    
+end
+
+ 
